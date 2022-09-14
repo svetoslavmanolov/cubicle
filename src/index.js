d8c5a3b1f126111c1b7f8430d1b1bdfc3578e63c
svetoslavmanolov <svn.manolov@gmail.com>
2022-09-14 09:07:02 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.use('/static', express.static('public')); //tova e kato middleware koito tarsi dali ima daden fail v public
+app.use('/static', express.static('public')); 
+
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs' 
+}));
+
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');  
+
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    
+    res.render('index');
 });
 
-app.listen(5000, () => console.log('App is listeting on port 5000...'));
+app.listen(5000, () => console.log('App is listening on port 5000...'));
