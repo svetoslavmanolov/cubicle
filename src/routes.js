d522694c3728011077c919e0aa82cb75006ecf2e
svetoslavmanolov <svn.manolov@gmail.com>
2022-09-15 18:13:56 +0300

    Add about page and fix navigation

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController.js');
 
 const router = express.Router();
 
-router.get('/', homeController.index);
+//tuk se izpolzvat dve konvencii 
+router.get('/', homeController.index);      //tuk kazvame koi action na koi route da otgovarq 
+router.get('/about', homeController.about);  //tuk kazvame koi action na koi route da otgovarq 
+
+router.use('/cube', cubeController);  // a tuk se delegira vsichki requests kam /cube da se zanimava s tqh kontrolera 
+
 
 module.exports = router;
 
