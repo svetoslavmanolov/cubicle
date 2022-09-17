e6eaa9c680dfee836f3f51e4e84a898e0d995968
svetoslavmanolov <svn.manolov@gmail.com>
2022-09-17 13:48:17 +0300

    Render static details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs/promises');
 const path = require('path');
+const cubeService = require('../services/cubeService');
 
 const cubes = require('../db.json');
 
@@ -8,7 +9,7 @@ router.get('/create', (req, res) => {
     res.render('create');
 })
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const cube = req.body;
 
     //Validate
@@ -16,20 +17,31 @@ router.post('/create', (req, res) => {
         return res.status(400).send('Invalid request!');
     }
     //Save data 
-    console.log(path.resolve('src', 'db.json'))
-    cubes.push(cube);
-    fs.writeFile(path.resolve('src', 'db.json'), JSON.stringify(cubes, '', 4), {encoding: 'utf-8'})
-    .then(() => {
+    try {
+        await cubeService.save(cube)
         //Redirect to page 
         res.redirect('/');
-    })
-
-    .catch(err => {
-        res.status(400).send(err);
-    });
-
-
-
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
+router.get('/details/:id', (req, res) => {
+    res.render('details')
+})
+
 module.exports = router;
+
+
+
+
+// cubes.push(cube);
+//     cubeService.save(cube)
+//     .then(() => {
+//         //Redirect to page 
+//         res.redirect('/');
+//     })
+
+//     .catch(err => {
+//         res.status(400).send(err);
+//     });
---
+++ src/services/cubeService.js
@@ -0,0 +1,14 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const cubes = require('../db.json');
+
+exports.save = (cube) => {
+    cubes.push(cube);
+    let textData = JSON.stringify(cubes, '', 4);
+    // console.log(path.resolve('src', 'db.json'))
+    return fs.writeFile(path.resolve('src', 'db.json'), textData, {encoding: 'utf-8'})
+    
+
+    
+}
